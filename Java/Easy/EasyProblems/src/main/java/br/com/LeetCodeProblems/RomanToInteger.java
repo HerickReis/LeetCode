035c84ce4c2b9ece390437876ea0d7733ad0f244
Herick <herickreis90.90@gmail.com>
2026-04-25 21:46:29 -0300

    vault backup: 2026-04-25 21:46:28

---
+++ Java/Easy/EasyProblems/src/main/java/br/com/LeetCodeProblems/RomanToInteger.java
@@ -0,0 +1,71 @@
+package br.com.LeetCodeProblems;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ Leia da esquerda para a direita:
+
+ Se o número atual for maior ou igual ao próximo → soma
+ Se o número atual for menor que o próximo → subtrai
+
+ I	1
+ V	5
+ X	10
+ L	50
+ C	100
+ D	500
+ M	1000
+
+
+ IV	4  -> 5 - 1
+ IX	9 -> 10 - 1
+
+
+ Example 1:
+
+ Input: s = "III"
+ Output: 3
+ Explanation: III = 3.
+
+
+ Example 3:
+
+ Input: s = "MCMXCIV"
+ Output: 1994
+ Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+
+ */
+
+
+public class RomanToInteger {
+
+    public int romanToInt(String s) {
+
+        return 1;
+    }
+
+    private int ConversionTable(String roman, int num) {
+        Map<String, Integer> RomanNumbers = Map.of(
+                "I", 1,
+                "V", 5,
+                "X", 10,
+                "L", 50,
+                "C", 100,
+                "D", 500,
+                "M", 1000
+        );
+
+
+
+        return RomanNumbers.get(roman);
+    }
+
+    private void ConversionRule(String romanExpression){
+
+
+
+    }
+
+}
